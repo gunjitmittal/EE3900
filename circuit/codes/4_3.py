04f2fa0281f39ed81cee031c53c5970c456c7d53
gunjitmittal <101900401+gunjitmittal@users.noreply.github.com>
2022-10-31 09:11:54 +0530

    Add files via upload

---
+++ circuit/codes/4_3.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def H(s):
+    return 5e5/(s + 1.5e6)
+
+S = np.linspace(-6e6, 3e6, 100)
+
+plt.plot(S, H(S))
+plt.grid()
+plt.title('Transfer Function')
+plt.xlabel('$s$')
+plt.ylabel('$H(s)$')
+plt.savefig('../figs/4.3.png')
