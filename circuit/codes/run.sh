591ba12ebb7c51a367da16f855357bcd38542da8
gunjitmittal <101900401+gunjitmittal@users.noreply.github.com>
2022-11-10 10:05:30 +0530

    Add files via upload

---
+++ circuit/codes/run.sh
@@ -0,0 +1,11 @@
+# wget https://raw.githubusercontent.com/gunjitmittal/EE3900/main/circuit/codes/2_8.cir
+# wget https://raw.githubusercontent.com/gunjitmittal/EE3900/main/circuit/codes/2_7.py
+ngspice 2_8.cir
+python3 2_7.py
+# wget https://raw.githubusercontent.com/gunjitmittal/EE3900/main/circuit/codes/3_5.cir
+# wget https://raw.githubusercontent.com/gunjitmittal/EE3900/main/circuit/codes/3_4.py
+ngspice 3_5.cir
+python3 3_4.py
+# wget https://raw.githubusercontent.com/gunjitmittal/EE3900/main/circuit/codes/4_3.py
+python3 4_3.py
+ngspice 4_7.cir
